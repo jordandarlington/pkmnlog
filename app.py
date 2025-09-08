d2f65ceec8f763399e45264b1c22d7214145acec
Jordan Darlington <jordandarlington@duck.com>
2025-09-08 20:51:47 +0100

    adds config & adds initial route

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import db, Playthrough, Entry
 from datetime import date
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pkmnlog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    playthroughs = Playthrough.query.order_by(Playthrough.start_date.desc()).all()
+    return render_template('index.html', playthroughs=playthroughs)
+
